397b88f1cff632bcdfe761fb6acf0dbbde232a78
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-14 14:35:56 +0200

    add click on square actions

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
       square.classList.add(shuffledArray[i])
       grid.appendChild(square)
       squares.push(square)
+
+      // normal click
+      square.addEventListener('click', function(e) {
+        click(square)
+      })
     }
 
     // add numbers
@@ -46,7 +51,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
   createBoard()
 
-
+  // click on square actions
+  function click(square) {
+    // if (isGameOver) return
+    if (square.classList.contains('checked') || square.classList.contains('flag')) return
+    if (square.classList.contains('bomb')) {
+      console.log('Game Over!')
+    } else {
+      let total = square.getAttribute('data')
+      if (total != 0) {
+        square.classList.add('checked')
+        square.innerHTML = total
+        return
+      }
+      square.classList.add('checked')
+    }
+  }
 
 
 
